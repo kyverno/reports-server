f367b4f166fbe9159e38120e00e6d236fa7f8e2b
Amit Tiwari <amit.tiwari@nirmata.com>
2025-02-17 10:37:35 +0530

    rerun tests - image multi arch fixed in ectd repo

--- fips.go
+++ fips.go
@@ -6,6 +6,7 @@ package main
 /*
 This file will only be compiled when BUILD_TAGS=fips.
 
+
 Package fipsonly enforces FIPS settings via init() function which
   1. Forces the application to use FIPS-compliant TLS configurations.
   2. Restricts cryptographic operations to those allowed under FIPS standards.
