f0b59b520462bc3f973c1c5011e51053ee79e5f2
Puneet Chelani <puneet.chelani@nirmata.com>
2025-04-03 16:57:38 +0530

    Add support for multiple urls for postgress connection

--- pkg/storage/db/new.go
+++ pkg/storage/db/new.go
@@ -4,10 +4,11 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"strings"
 	"time"
 
+	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/kyverno/reports-server/pkg/storage/api"
-	_ "github.com/lib/pq"
 	"k8s.io/klog/v2"
 )
 
@@ -18,7 +19,7 @@ const (
 
 func New(config *PostgresConfig, clusterId string) (api.Storage, error) {
 	klog.Infof("starting postgres db")
-	db, err := sql.Open("postgres", config.String())
+	db, err := sql.Open("pgx", config.String())
 	if err != nil {
 		klog.Error("failed to open db", err.Error())
 		return nil, err
@@ -122,7 +123,36 @@ type PostgresConfig struct {
 }
 
 func (p PostgresConfig) String() string {
-	return fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=%s sslrootcert=%s sslkey=%s sslcert=%s",
-		p.Host, p.Port, p.User, p.Password, p.DBname, p.SSLMode, p.SSLRootCert, p.SSLKey, p.SSLCert)
+	// Handle multiple hosts if Host contains space-separated values
+	hosts := strings.Fields(p.Host)
+	var hostList []string
+	
+	for _, h := range hosts {
+		hostList = append(hostList, fmt.Sprintf("%s:%d", h, p.Port))
+	}
+	
+	// If no hosts found, use the Host field directly
+	if len(hostList) == 0 && p.Host != "" {
+		hostList = append(hostList, fmt.Sprintf("%s:%d", p.Host, p.Port))
+	}
+	
+	// Join hosts with commas for pgx URL format
+	hostStr := strings.Join(hostList, ",")
+
+	// Build URL format connection string
+	url := fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=%s",
+		p.User, p.Password, hostStr, p.DBname, p.SSLMode)
+
+	// Add SSL parameters if provided
+	if p.SSLRootCert != "" {
+		url += fmt.Sprintf("&sslrootcert=%s", p.SSLRootCert)
+	}
+	if p.SSLKey != "" {
+		url += fmt.Sprintf("&sslkey=%s", p.SSLKey)
+	}
+	if p.SSLCert != "" {
+		url += fmt.Sprintf("&sslcert=%s", p.SSLCert)
+	}
+
+	return url
 }
