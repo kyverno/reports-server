84e9902bb4287ad1f8c87ce1b561c012dac0e89b
aerosouund <aerosound161@gmail.com>
2025-07-19 19:42:00 +0300

    update migrations
    Signed-off-by: aerosouund <aerosound161@gmail.com>

--- pkg/storage/db/migrations/000006_add_clusterid_reports_tables.down.sql
+++ pkg/storage/db/migrations/000006_add_clusterid_reports_tables.down.sql
@@ -2,5 +2,3 @@ ALTER TABLE clusterephemeralreports DROP COLUMN cluster_id;
 ALTER TABLE ephemeralreports DROP COLUMN cluster_id;
 ALTER TABLE clusterpolicyreports DROP COLUMN cluster_id;
 ALTER TABLE policyreports DROP COLUMN cluster_id;
-ALTER TABLE clusterreports DROP COLUMN cluster_id;
-ALTER TABLE reports DROP COLUMN cluster_id;
--- pkg/storage/db/migrations/000007_update_indexes_reports_tables.up.sql
+++ pkg/storage/db/migrations/000007_update_indexes_reports_tables.up.sql
@@ -2,8 +2,6 @@ ALTER TABLE clusterephemeralreports DROP CONSTRAINT clusterephemeralreports_pkey
 ALTER TABLE clusterephemeralreports ADD PRIMARY KEY (cluster_id, name);
 ALTER TABLE clusterpolicyreports DROP CONSTRAINT clusterpolicyreports_pkey;
 ALTER TABLE clusterpolicyreports ADD PRIMARY KEY (cluster_id, name);
-ALTER TABLE clusterreports DROP CONSTRAINT clusterreports_pkey;
-ALTER TABLE clusterreports ADD PRIMARY KEY (cluster_id, name);
 ALTER TABLE ephemeralreports DROP CONSTRAINT ephemeralreports_pkey;
 ALTER TABLE ephemeralreports ADD PRIMARY KEY (cluster_id, name, namespace);
 CREATE INDEX IF NOT EXISTS ephemeralreports_namespace ON ephemeralreports(cluster_id, namespace);
@@ -11,8 +9,4 @@ DROP INDEX IF EXISTS ephemeralreportnamespace;
 ALTER TABLE policyreports DROP CONSTRAINT policyreports_pkey;
 ALTER TABLE policyreports ADD PRIMARY KEY (cluster_id, name, namespace);
 CREATE INDEX IF NOT EXISTS policyreports_namespace ON policyreports(cluster_id, namespace);
-DROP INDEX IF EXISTS policyreportnamespace;
-ALTER TABLE reports DROP CONSTRAINT reports_pkey;
-ALTER TABLE reports ADD PRIMARY KEY (cluster_id, name, namespace);
-CREATE INDEX IF NOT EXISTS reports_namespace ON reports(cluster_id, namespace);
-DROP INDEX IF EXISTS reportnamespace;
+DROP INDEX IF EXISTS policyreportnamespace;s
