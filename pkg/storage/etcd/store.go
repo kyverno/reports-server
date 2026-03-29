4a30ec18af68467174d1abea2b773d183417953e
aerosouund <aerosound161@gmail.com>
2026-03-29 19:18:22 +0200

    fix lint
    Signed-off-by: aerosouund <aerosound161@gmail.com>

--- pkg/storage/etcd/store.go
+++ pkg/storage/etcd/store.go
@@ -167,6 +167,7 @@ func (o *objectStoreNamespaced[T]) Delete(ctx context.Context, name, namespace s
 func (o *objectStoreNamespaced[T]) Count(ctx context.Context) (int, error) {
 	return 0, fmt.Errorf("count not implemented for etcd objectStoreNamespaced")
 }
+
 type ObjectStorageCluster[T metav1.Object] interface {
 	Get(ctx context.Context, name string) (T, error)
 	List(ctx context.Context) ([]T, error)
