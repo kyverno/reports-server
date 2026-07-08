707f9d2c29613178f9ab3656bb64a4a606e63306
Arpit Jain <arpitjain099@gmail.com>
2026-07-08 09:30:03 +0900

    test(etcd): drop the explanatory comment on the cluster-store regression test
    Signed-off-by: Arpit Jain <arpitjain099@gmail.com>

--- pkg/storage/etcd/store_test.go
+++ pkg/storage/etcd/store_test.go
@@ -7,11 +7,6 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
-// TestClusterStoreUseResourceVersionNoPanic guards against a regression where
-// NewObjectStoreCluster left the embedded *versioning.ResourceVersion nil. The
-// first call to UseResourceVersion (made during Create of a cluster-scoped
-// report) then dereferenced that nil pointer and panicked, taking down the
-// apiserver request handler. See kyverno/reports-server#352.
 func TestClusterStoreUseResourceVersionNoPanic(t *testing.T) {
 	gvk := schema.GroupVersionKind{Group: "wgpolicyk8s.io", Version: "v1alpha2", Kind: "ClusterPolicyReport"}
 	gr := schema.GroupResource{Group: "wgpolicyk8s.io", Resource: "clusterpolicyreports"}
